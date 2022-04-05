d25f1a32d5834e79dd957ac4cbaef20d3bad2b8c
rajib <rajibislam700@gmail.com>
2022-04-05 12:28:49 +0600

    tailwind practice

---
+++ src/App.js
@@ -0,0 +1,48 @@
+function App() {
+  return (
+    <div className="bg-gray-800 md:flex justify-between">
+      <div className="app-container mb-5 md:mb-0">
+        <h1 className="text-rajib">ChitChat</h1>
+        <p className="text-white text-body">You have a new message!</p>
+        <div className="">
+          <button className="btn btn-green">View Message</button>
+          <button className="btn btn-red">Delete Message</button>
+        </div>
+      </div>
+      <div className="app-container mb-5 md:mb-0">
+        <h1 className="text-rajib">ChitChat</h1>
+        <p className="text-white text-body">You have a new message!</p>
+        <div className="">
+          <button className="btn btn-green">View Message</button>
+          <button className="btn btn-red">Delete Message</button>
+        </div>
+      </div>
+      <div className="app-container mb-5 md:mb-0">
+        <h1 className="text-rajib">ChitChat</h1>
+        <p className="text-white text-body">You have a new message!</p>
+        <div className="">
+          <button className="btn btn-green">View Message</button>
+          <button className="btn btn-red">Delete Message</button>
+        </div>
+      </div>
+      <div className="app-container mb-5 md:mb-0">
+        <h1 className="text-rajib">ChitChat</h1>
+        <p className="text-white text-body">You have a new message!</p>
+        <div className="">
+          <button className="btn btn-green">View Message</button>
+          <button className="btn btn-red">Delete Message</button>
+        </div>
+      </div>
+      <div className="app-container mb-5 md:mb-0">
+        <h1 className="text-rajib">ChitChat</h1>
+        <p className="text-white text-body">You have a new message!</p>
+        <div className="">
+          <button className="btn btn-green">View Message</button>
+          <button className="btn btn-red">Delete Message</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
